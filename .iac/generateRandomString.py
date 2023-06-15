fa28d90a27ccdea31dbbdd513398ed5b5102ac15
gintautasm <gintas@musketas.lt>
2023-06-15 20:33:20 +0300

    renamed directory

--- .iac/generateRandomString.py
+++
@@ -1,5 +0,0 @@
-# create function that returns random string of specific length
-import random
-
-def random_string(length=10):
-    return ''.join(random.choice('abcdefghijklmnopqrstuvwxyz0123456789') for i in range(length))
